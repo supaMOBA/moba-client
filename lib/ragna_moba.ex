a272b2a556eeb349c18653b7584d9f35e4d26a09
Alexandre de Souza <alexandre@aledsz.com.br>
2021-09-10 08:24:35 -0300

    :sparkles: add local configs dynamic path

--- lib/ragna_moba.ex
+++ lib/ragna_moba.ex
@@ -6,4 +6,22 @@ defmodule RagnaMoba do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @env Mix.env()
+
+  @doc """
+  Returns the config path on system disk.
+  """
+  def config_path do
+    if Desktop.OS.windows?() do
+      Path.join([Desktop.OS.home(), "Documents", "ragnaMOBA"])
+    else
+      Path.join([Desktop.OS.home(), ".config", "ragnaMOBA"])
+    end
+  end
+
+  @doc """
+  Returns the application environment
+  """
+  def env, do: @env
 end
